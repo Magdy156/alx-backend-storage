b8607c89fdcee3e879927577c968bd0c524acbfb
Magdy156 <magdyhamdy156@gmail.com>
2024-05-07 02:59:47 +0300

    task0

---
+++ 0x02-redis_basic/exercise.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+"""
+Redis client module
+"""
+import redis
+from uuid import uuid4
+from typing import Union
+
+
+class Cache:
+    """
+    Caching class
+    """
+    def __init__(self) -> None:
+        """
+        Initialize new cache object
+        """
+        self._redis = redis.Redis()
+        self._redis.flushdb()
+
+    def store(self, data: Union[str, bytes,  int,  float]) -> str:
+        """
+        Stores data in redis with randomly generated key
+        """
+        key = str(uuid4())
+        client = self._redis
+        client.set(key, data)
+        return key
